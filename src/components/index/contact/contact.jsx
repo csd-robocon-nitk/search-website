44abe7fb8cc77c2b2f93d732c503cae4529a31bd
Testing wala User <apoorva3569@gmail.com>
2024-01-17 19:54:19 +0530

    Google Form enabled on Contact Us Section

--- src/components/index/contact/contact.jsx
+++ src/components/index/contact/contact.jsx
@@ -1,73 +1,57 @@
 import React, { useEffect, useState } from 'react'
 import './contact.css'
 import image from '../../../images/svg-3.svg'
-import axios from 'axios'
+// import axios from 'axios'
 import 'animate.css'
 
-export default function ContactSection () {
+export default function ContactSection() {
+	let [ name, setName ] = useState("")
+	let [ email, setEmail ] = useState("")
+	let [ message, setMessage ] = useState("")
 
-    let [name, setName] = useState("")
-    let [email, setEmail] = useState("")
-    let [message, setMessage] = useState("")
     let [modalOpen, setModalOpen] = useState(false)
 
-    useEffect(() => {
-        document.body.style.overflow = "hidden auto"
-    }, [])
+    // useEffect(() => {
+    //     document.body.style.overflow = "hidden auto"
+    // }, [])
 
-    useEffect(() => {
-        document.body.style.overflow = modalOpen ? "hidden" : "hidden auto"
-    }, [modalOpen])
+    // useEffect(() => {
+    //     document.body.style.overflow = modalOpen ? "hidden" : "hidden auto"
+    // }, [modalOpen])
 
-    let submitHandler = async (event) => {
-        event.preventDefault();
-        let formData = {
-            name: name,
-            email: email,
-            message: message
-        }
-        // sheet url = 'https://docs.google.com/spreadsheets/d/1BeIkPnzWcrgL8jdpF5ob1ZmWH0IVqIJ0axIt5Zngpag/edit?usp=sharing'
-        let response = await axios.post(
-            'https://script.google.com/macros/s/AKfycbz0ktzlgtbsJg2Al4A6EInN_MzE6LE6K6mgMakTxglGEYE4cHQRKo2LtxtAxlhNJH43NA/exec',
-            JSON.stringify(formData)
-        )
-
-        setName("");
-        setEmail("");
-        setMessage("");
-        setModalOpen(true)
-        document.body.style.overflow = "hidden"
-        document.querySelector('.modal-container').style.top = document.body.scrollTop + 80 + "px"
-    }
-    return (
-        <>
+	return (
+        <>		
             <div id="contact" className="light">
                 <div id="left">
-                    <h1>CONTACT US</h1>
-                    <h2>Get in touch with us</h2>
-            
-                    <form className="dark" onSubmit={submitHandler}>
+                    <div>
+                        <h1>CONTACT US</h1>
+                        <h2>Get in touch with us</h2>
+                    </div>
+                <form action='https://docs.google.com/forms/d/e/1FAIpQLSfFwlCw13XaKxwaFqxH5dekZnG4fJyC-mMpdtLNgqKjjQm17A/formResponse'
+                        target='form_res'
+                        method='GET'
+                        className="dark"
+                    >
                         <label htmlFor="name">Name</label>
-                        <br/>
-                        <input id="name" value={name} type="text" onChange={(e) => setName(e.target.value)} required />
-                        <br/>
-            
+                        {/* <br/> */}
+                        <input id="name" value={name} type="text" onChange={(e) => setName(e.target.value)} name='entry.638404753' required />
+                        {/* <br/> */}
                         <label htmlFor="email">Email</label>
-                        <br/>
-                        <input id="email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} required />
-                        <br/>
+                        {/* <br/> */}
+                        <input id="email" value={email} type="email" onChange={(e) => setEmail(e.target.value)} name='entry.776579162' required />
+                        {/* <br/> */}
                         <label htmlFor="message">Message</label>
-                        <br/>
-                        <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required />
-                        <br/>
+                        {/* <br/> */}
+                        <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} name='entry.2082912176' required />
+                        {/* <br/> */}
                         <button type="submit" className='button'>Submit</button>
                     </form>
+                    <iframe src="" title="form response" name="form_res" style={{display:'none'}} ></iframe>
                 </div>
                 <div id="right">
                     <img src={image} />
                 </div>
             </div>
-
             <div className="modal-container light"  style={{display: modalOpen ? "flex" : "none"}}>
                 <div className={`modal ${modalOpen && "animate__animated animate__zoomIn animate__faster"}`}>
                     <h3>Your response has been submitted!</h3>
@@ -76,5 +60,6 @@ export default function ContactSection () {
                 </div>
             </div>
         </>
-    )
+
+	)
 }
