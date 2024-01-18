026020629ceeea8475f3d143734a5d06f751b796
Testing wala User <apoorva3569@gmail.com>
2024-01-18 19:34:10 +0530

    Gallery fixed

--- src/components/pages/gallery.jsx
+++ src/components/pages/gallery.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './gallery.css'
-
-const listext=[".jpg",".jpeg",".JPG"]
+import gallery_data from '../../gallery_data'
 
 export default function Gallery () {
     return (
@@ -11,17 +10,8 @@ export default function Gallery () {
                 <h2>Gallery</h2>
                 <div id="grid">
                     {
-                        (new Array(31)).fill(0).map((el, i) => {
-                            let n = 2-Math.floor(Math.log(i+1)/Math.log(10))
-                            let x = ""
-                            for (let i=0 ; i<n ; i++) x += "0"
-                            x += i+1
-                            let path = `/images/gallery/${x}.jpg`
-                            path = `/images/gallery/${x}${listext[0]}`
-                            path = `/images/gallery/${x}${listext[1]}`
-                            path = `/images/gallery/${x}${listext[2]}`
-                            
-                            return <img src={path}  key={i} />
+                        gallery_data.map((path, i) => {
+                            return <img src={path.id}  key={i} />
                         })
                     }
                 </div>
